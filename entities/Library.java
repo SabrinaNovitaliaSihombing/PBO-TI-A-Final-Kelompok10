8be639f927a8498f12290792d75c27c8f8b8212d
Daniel Hutabarat <2381015@unai.edu>
2024-12-02 09:35:02 +0700

    entities java

---
+++ entities/Library.java
@@ -0,0 +1,25 @@
+package entities;
+
+public class Library {
+    private String library;
+    private Integer id;
+
+    public Library() {
+    }
+
+    public String getLibrary() {
+        return library;
+    }
+
+    public void setLibrary(final String library) {
+        this.library = library;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(final Integer id) {
+        this.id = id;
+    }
+}
